b3077b08168a70cca9294c3caf2585121d10a3e2
Orkh4n <orxan.ali90@gmail.com>
2022-05-01 20:08:52 +0400

    first commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name");
+            var name=Console.ReadLine().ToUpper();
+            Console.WriteLine("Enter Surname");
+            var surname=Console.ReadLine().ToUpper();
+            Console.WriteLine(name +" "+ surname);
+            Console.WriteLine("enter first letter");
+            var first = Console.ReadLine();
+            Console.WriteLine("enter second letter");
+            var second = Console.ReadLine();
+            Console.WriteLine("enter third letter");
+            var third = Console.ReadLine();
+            Console.WriteLine(third + second + first);
+        }
+    }
+}
